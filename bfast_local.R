72120716ad36ccd7ec03006c2cdcd3be88c2aca6
Peter James Zellner <peterjames.zellner@eurac.edu>
2021-08-02 10:23:33 +0000

    got bfast spatial to run

--- bfast_local.R
+++ bfast_local.R
@@ -17,32 +17,61 @@ library(ggplot2)
 
 # storms -----------------------------------------------------------------------
 
+pth_stt = "/mnt/CEPH_BASEDATA/GIS/REGIONAL/SOUTHTYROL/BOUNDARIES/SouthTyrol/Bezirksgemeinschaften_WGS84.shp"
+stt = st_read(pth_stt)
+stt = st_transform(stt, crs = 32632)
+
+
 # large storm archive ----
 # # this shapefile is invalid, can't be used with sf
 # # has attributes
 # pth_wind = "/mnt/CEPH_PROJECTS/ECO4Alps/Forest_Disturbances/01_data/01_reference_data/9555008/FORWIND_v2.shp"
 # wind = st_read(pth_wind)
-# wind = wind %>% dplyr::filter(StormName == "Vaia")
+# # wind = wind %>% dplyr::filter(StormName == "Vaia")
+# wind = wind %>% dplyr::filter(Country == "IT")
+# 
 # wind = st_transform(wind, crs = 32632)
+# wind = st_intersection(x = wind, y = st_as_sfc(st_bbox(stt)))
+# 
 # wind = st_buffer(wind, 0)
 # wind = st_make_valid(wind)
 # #table(st_is_valid(wind))
 # #wind_area = st_intersection(wind[3, ], st_transform(area, crs = st_crs(wind)))
 # 
-# wind = wind %>% select(id = Id_poly, event_date = EventDate, 
+# wind = wind %>% select(id = Id_poly, event_date = EventDate,
 #                        storm_name = StormName, area = Area, damage_degree = Damage_deg)
 # wind = wind %>% mutate(event_date = gsub(pattern = "/", replacement = "-", x = event_date))
+# table(wind$event_date)
 # ----
 
 # couple of windthrow areas welschenofen
 pth_area = "/mnt/CEPH_PROJECTS/ECO4Alps/Forest_Disturbances/01_data/01_reference_data/area_32632.shp"
 area = st_read(pth_area)
-mapview(st_bbox(area))
 area_bbox = st_bbox(area)
 
-# s2 ndvi masked with fmask ----------------------------------------------------
+# no disturbance manually
+library(mapedit)
+no_dist = mapedit::drawFeatures()
+no_dist = no_dist %>% st_transform(st_crs(area))
 
-# file list
+mapview(st_bbox(area)) + mapview(area) + mapview(st_bbox(stt)) + mapview(no_dist, col.reg = "red")
+
+
+# forest mask
+pth_forest = "/mnt/CEPH_PROJECTS/ECO4Alps/Land_Cover/Land_cover_data/HR_Layer/FTY_2018_010m_32632.tif"
+forest = stars::read_stars(pth_forest, proxy = TRUE)
+forest = forest[st_bbox(area)]
+mapview(st_bbox(forest))
+forest = st_as_stars(forest)
+plot(forest)
+forest[[1]][forest[[1]] == 0] = NA
+forest[[1]][forest[[1]] == 2] = 1
+
+# use this for masking
+
+
+# s2 ndvi masked with fmask and forest hrl -------------------------------------
+# file list ndvi fmask
 path_ndvi = "/mnt/CEPH_PROJECTS/ECO4Alps/Forest_Disturbances/01_data/02_s2_ndvi_local_eurac/ndvi_novalevante_fmask"
 fls_ndvi = list.files(path_ndvi, full.names = TRUE)
 fls_ndvi = tibble(pth = fls_ndvi, 
@@ -59,13 +88,21 @@ ndvi_prox = stars::st_set_dimensions(.x = ndvi_prox, which = "t",
 # st_dimensions(ndvi)
 # st_get_dimension_values(ndvi, "t")
 
+
+
+
+
 # subset spatially
 # THIS IS WHERE TO LOOP THROUGH AREA
-ndvi_prox = ndvi_prox[st_bbox(area[1, ])]
+aoi = area[1, ]
+aoi = no_dist
+aoi_bbox = st_bbox(aoi)
+
+ndvi_prox = ndvi_prox[aoi_bbox]
 
 # read to r
 ndvi = st_as_stars(ndvi_prox)
-mapview(st_bbox(ndvi)) + mapview(area[1, ])
+mapview(st_bbox(ndvi)) + mapview(aoi)
 
 # create mask
 fmask = ndvi %>% slice("band", 2)
@@ -79,17 +116,19 @@ ndvi_msk = ndvi_msk * fmask
 #plot(ndvi_msk %>% slice("t", 200))
 
 # ndvi msk has the correct values
+ndvi_msk_valpx = st_apply(ndvi_msk, c("x","y"), function(x){
+  sum(!is.na(x))})
+plot(ndvi_msk_valpx)
 # look what happens with them from here!
 
-
 # get mean ndvi for windthrow poly in 2017-2019
-ndvi_msk_cut = ndvi_msk[area[1, ]]
+ndvi_msk_cut = ndvi_msk[aoi]
 ndvi_msk_cut[[1]][ndvi_msk_cut[[1]] <= 0] = NA
 ndvi_msk_cut = st_apply(ndvi_msk_cut, c("t"), median, na.rm = TRUE) # 
 ndvi_ts = tibble(ndvi = ndvi_msk_cut %>% pull() %>% c(), 
                  date = st_get_dimension_values(ndvi_msk_cut, "t"))
 
-ggplot(ndvi_ts %>% filter(lubridate::year(date) == 2019), aes(x=date, y=ndvi)) +
+ggplot(ndvi_ts %>% filter(lubridate::year(date) == 2020), aes(x=date, y=ndvi)) +
   geom_line() +
   geom_point()
 
@@ -97,56 +136,34 @@ ggplot(ndvi_ts, aes(x=date, y=ndvi)) +
   geom_line() +
   geom_point()
 
-ndvi_dates = st_get_dimension_values(ndvi, "t")
+
+# bfast on one pixel
+ndvi_dates = st_get_dimension_values(ndvi_msk, "t")
 diff.Date(ndvi_dates)
+pixels = ndvi_msk[st_point_on_surface(aoi)] %>% pull() %>% c()
+summary(pixels)
 
-pixels = ndvi[st_point_on_surface(area[1, ])] %>% pull() %>% c()
-summary(pixels) # where does 255 come from
-pixels[pixels > 1] = NA
-pixels[pixels < 0] = NA
+lsts <- bfastts(pixels, ndvi_dates, type = c("irregular"))
+plot(lsts)
 
-lsts <- bfastts(pixels, ndvi_dates, type = c("irregular")) # what is this doing is it changing, adding values? compare values to input values!
 
 # lsts_lin = round(na.approx(lsts), 4)
 # lsts_per = round(na.interp(lsts), 4) # bfast monitor not needed!!!!!
-
-
-# aggregate.daily.to.weekly <- function(daily.ts) {
-#   
-#   dates      <- as.Date(date_decimal(as.numeric(time(daily.ts))))
-#   
-#   xts.daily  <- xts(daily.ts, order.by = dates)
-#   
-#   xts.weekly <- round(xts::apply.weekly(xts.daily, median),4)  # xts
-#   
-#   start(xts.weekly)
-#   ts.weekly <- ts(data = xts.weekly, 
-#                   # define the start and end (Year, Week)    
-#                   start = c(as.numeric(format(start(xts.weekly), "%Y")),
-#                             as.numeric(format(start(xts.weekly), "%W"))), 
-#                   end   = c(as.numeric(format(end(xts.weekly), "%Y")), 
-#                             as.numeric(format(end(xts.weekly), "%W"))), 
-#                   frequency = 52)
-#   
-#   return(ts.weekly)
-# }
-# lsts_week = aggregate.daily.to.weekly(lsts_per)
-
 plot(lsts)
-plot(lsts_lin)
-plot(lsts_per)
-plot(lsts_week)
+# plot(lsts_lin)
+# plot(lsts_per)
+# plot(lsts_week)
 
 
 bfm_res = bfastmonitor(lsts, 2018, 
                        formula = response~harmon,# response~season #response~trend, # response~trend+harmon, response~harmon
-                       order = 1, 
+                       order = 1, # decide 1 or 2 
                        history = "all", #date could be specified e.g one year, all = history without breaks, don't have disturbance in history
                        verbose = T)
 # harmon order = 3, reducing will lead to less seasonality = waves per year -> for forrest maybe 2 or 1
 # season sbins = 3, num of seasonal dummies -> 4 
 
-plot(bfm_res) # where do all these 1 and 0 come from?
+plot(bfm_res) # where do all these 1 and 0 come from? # THE TS LÖÖKS LIKE SHIT WITH ALL 1 and 0???
 
 
 # IMPROVE ERROR HANDLING, CHECK LENGHT OF NON NA AND WRITE TO FILE
@@ -155,59 +172,32 @@ SpatialBFM = function(pixels)
 {
   #lsts = ts(pixels, c(2016, 1), frequency=30.666667)
   lsts = bfastts(pixels, ndvi_dates, type = c("irregular")) # check how this looks then
-  #lsts = round(na.approx(lsts), 4)
-  print(length(lsts))
-  if (length(lsts) < 50){
+  if (sum(!is.na(lsts)) < 100){
     return(NA)
   }
   bfastmonitor(lsts, 2018, formula=response~harmon, order = 1, history = "all", 
-               verbose = T)$breakpoint
+               verbose = F)$breakpoint
 }
 
 
-valid_obs = st_apply(ndvi_msk[area[1, ]], c("x", "y"), function(x){length(!is.na(x))}, PROGRESS=TRUE)
+plot(ndvi_msk %>% slice("t", 207))
+valid_obs = st_apply(ndvi_msk, c("x", "y"), function(x){sum(!is.na(x))}, PROGRESS=TRUE)
+plot(valid_obs)
+StarsResult = st_apply(ndvi_msk, c("x", "y"), SpatialBFM, PROGRESS = FALSE)
+StarsResult
+plot(StarsResult)
 
-StarsResult = st_apply(ndvi_msk[area[1, ]], c("x", "y"), SpatialBFM, PROGRESS=TRUE)
-
-
-
-
-bf_res <- bfast(lsts_week, h = 10/length(lsts_per), 
-                season = "harmonic", breaks = 1, max.iter = 2)
-plot(bf_res)
 
 # forest -----------------------------------------------------------------------
-pth_forest = "/mnt/CEPH_PROJECTS/ECO4Alps/Land_Cover/Land_cover_data/HR_Layer/FTY_2018_010m_32632.tif"
-forest = stars::read_stars(pth_forest, proxy = TRUE)
-forest = forest[st_bbox(fmask)]
-mapview(st_bbox(forest))
+
 
 # forest density ---------------------------------------------------------------
 
 
 
 
-# fmask ------------------------------------------------------------------------
-pth_fmask = "/mnt/CEPH_PRODUCTS/FMASK/SENTINEL2/T32TPS/S2A_MSIL1C_20150704T101006_N0204_R022_T32TPS_20150704T101337.SAFE/L1C_T32TPS_A000162_20150704T101337_Fmask4.tif"
-fmask = read_stars(pth_fmask)
-plot(fmask)
-fmask[[1]][fmask[[1]] > 0] =  1
-
-mapview(st_bbox(fmask)) + mapview(st_bbox(wind))
-
-st_crs(fmask)
-wind_fmask = st_intersection(x = st_as_sfc(st_bbox(fmask)), y = wind)
-
-nrow(wind)
-length(wind_fmask)
-mapview(st_bbox(wind_fmask)) + mapview(wind_fmask[1:5])
-
-# make file list and compare dates!
 
 
-
-
-# apply bfast ------------------------------------------------------------------
 
 
 
