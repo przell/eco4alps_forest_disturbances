e16c0241d191db6548898fe3b675582c8282be8d
Peter James Zellner <peterjames.zellner@eurac.edu>
2021-08-04 19:01:10 +0000

    wrote reporting functions for bfast breakpoint stars ouptut

---
+++ d2.1_accuracy_bfast/bfast_accuracy.R
@@ -0,0 +1,223 @@
+# bfast accuracy reporting
+
+# the brks from bfast come like this:
+# - detected_break = decimal year (2018.5)
+# - no_break = 0
+# - no_forest = NA
+
+# bfast reporter
+
+# input 
+# bfast results (brks, val_obs, magn)
+# forest mask
+# vaja storm damage shps
+# control shps
+
+# workflow 
+# loop through damage shps 
+# (loop through control shps)
+
+# indicators
+# count total pixels = forest pixels in aoi
+# count total observation dates
+# count valid observations (per year, per season) -> boxplot
+# count snow, cloud pixels (from mask)
+# get ndvi ts for aoi mean
+# get bfast breakpoints for aoi
+# % before
+# % break_lag: 1, 2, 3, 6 , >6 months
+# % no_break
+# get magnitude 
+
+# later in project checks:
+# winter
+# walddichte 2-3 klassen
+# datendichte anzahl der punkte
+# spatial resolution 3x3 vs pixel
+# newer events then 2018
+# parameters (h, magnitude) to not identify false positives
+# select 10 clean disturbance events and 10 clean non-disturbance patches
+# -> add aggregated patch stats 
+# -> analyse on patch basis also with ndvi time series and breakpoint
+
+# libs ------------------------------------------------------------------------
+library(stars)
+library(sf)
+library(mapview)
+library(dplyr)
+library(stringr)
+library(parallel)
+library(bfast)
+library(pbapply)
+library(lubridate)
+library(xts)
+library(zoo)
+library(forecast)
+library(ggplot2)
+
+
+# data ------------------------------------------------------------------------
+pth_res = "/mnt/CEPH_PROJECTS/ECO4Alps/Forest_Disturbances/03_results/"
+pth_brks = paste0(pth_res, "brks.tif")
+pth_magn = paste0(pth_res, "magn.tif")
+pth_val_obs = paste0(pth_res, "val_obs.tif")
+
+brks = read_stars(pth_brks)
+magn = read_stars(pth_magn)
+val_obs = read_stars(pth_val_obs)
+
+pth_forest = "/mnt/CEPH_PROJECTS/ECO4Alps/Forest_Disturbances/01_data/01_reference_data/FTY_2018_030m_binary_ndvi_mask.tif"
+forest = read_stars(pth_forest)
+
+pth_wind = "/mnt/CEPH_PROJECTS/ECO4Alps/Forest_Disturbances/01_data/01_reference_data/area_32632.shp"
+wind = st_read(pth_wind)
+
+
+# function analyser ------------------------------------------------------------
+# function to analyse breakpoint of bfastmonitor output stars object
+analyse_brks = function(brks, event_date = NULL){
+  # get total count and no_brk count
+  cnt = list(
+    cnt_total = length(brks[[1]]),
+    cnt_forest = sum(!is.na(brks[[1]])),
+    cnt_noforest = sum(is.na(brks[[1]])),
+    cnt_brk = sum(brks[[1]] > 0, na.rm = T),
+    cnt_no_brk = sum(brks[[1]] == 0, na.rm = T)
+    )
+  
+  # test
+  stopifnot(cnt$cnt_forest + cnt$cnt_noforest == cnt$cnt_total)
+  stopifnot(cnt$cnt_brk + cnt$cnt_no_brk == cnt$cnt_forest)
+  
+  # set no_break to na to not interfere with further analysis
+  brks[[1]][brks[[1]] == 0] = NA 
+  
+  # pull data
+  brks_v = as.vector(brks$brks.tif)
+  
+  # quantiles
+  quantiles = date_decimal(quantile(brks_v, na.rm = T)) %>% as.Date()
+  
+  # hist(brks_v)
+  yrs = sort(unique(round(na.omit(brks_v),0)))
+  n_yrs = length(yrs)
+  plt_hist = ggplot(data.frame(brks = brks_v), aes(x=brks)) + 
+    geom_histogram(color="black", fill="white", 
+                   breaks=seq(from = min(yrs), to = max(yrs), by= 0.25))
+  
+  # boxplot(brks_v)
+  plt_boxplot = ggplot(data.frame(brks = brks_v), aes(x = "1", y = brks)) + 
+    geom_boxplot() + 
+    stat_summary(geom="text", fun=quantile,
+                 aes(label=sprintf("%1.1f", ..y..)),
+                 position=position_nudge(x=0.5), size=3.5) 
+  
+  # assemble results in list
+  res = list(
+    "cnt" = cnt, 
+    "quantiles" = quantiles, 
+    "plt_hist" = plt_hist, 
+    "plt_boxplot" = plt_boxplot
+  )
+  
+  # event part only if event is specified
+  if(!is.null(event_date)){
+    event_date = as.Date(event_date)
+    min_brk = min(brks_v, na.rm = T) %>% date_decimal() %>% as.Date()
+    max_brk = max(brks_v, na.rm = T) %>% date_decimal() %>% as.Date()
+    event_date = as.Date(event_date)
+    date_cls = c(min_brk-1,
+                 event_date, 
+                 event_date+31, 
+                 event_date+31*2, 
+                 event_date+31*3, 
+                 event_date+31*6, 
+                 event_date+31*12, 
+                 max_brk+1)
+    date_cls_dec = decimal_date(date_cls)
+    date_labels = c("before", "1mnth", "2mnth", "3mnth", "6mnth", "12mnth", ">12mnth")
+    
+    cls = cut(brks_v, breaks = date_cls_dec, labels = c(date_labels))
+    acc_table = as.data.frame(table(cls), stringsAsFactors = FALSE)
+    acc_table$date_cls = date_cls[-1]
+    acc_table$date_cls_dec = date_cls_dec[-1]
+    acc_table = rbind(acc_table, 
+                      data.frame(cls = "no_break", Freq = cnt$cnt_no_brk, 
+                                 date_cls = NA, date_cls_dec = NA))
+    
+    stopifnot(sum(acc_table$Freq) == cnt$cnt_forest)
+    
+    acc_table = acc_table %>% 
+      mutate(freq_perc = Freq/sum(Freq)*100) %>% 
+      rename(freq = Freq)
+    
+    plt_acc_pie = ggplot(acc_table, aes(x="", y=freq, fill=cls)) +
+      geom_bar(width = 1, stat = "identity") +
+      coord_polar("y", start=0)  
+    
+    # add vline to hist where event is
+    event_date_dec = decimal_date(event_date)
+    plt_hist = plt_hist + geom_vline(xintercept = event_date_dec, color = "red")
+    
+    # make special hist with brakes as defined in acc_table
+    plt_hist_event = ggplot(data.frame(brks = brks_v), aes(x=brks)) + 
+      geom_histogram(color="black", fill="white", 
+                     breaks=date_cls_dec) +
+      geom_vline(xintercept = event_date_dec, color = "red")
+    
+    # assemble in list
+    res$plt_hist = plt_hist # overwrite old plot
+    res = c(res, 
+            list("event_table" = acc_table, 
+                 "plt_pie_event" = plt_acc_pie,
+                 "plt_hist_event" = plt_hist_event))
+  }
+  return(res)
+}
+
+
+# analysis ---------------------------------------------------------------------
+# get values in wind areas
+wind = st_transform(wind, crs = st_crs(brks))
+brks_wind = brks[wind]
+plot(brks_wind) # 0 = no brk, NA = forest! 
+anal_brks_wind = analyse_brks(brks = brks_wind, event_date = "2018-10-27")
+names(anal_brks_wind)
+
+# color this guy according to validity:
+# 1 - 12 mnth: green to yellow
+# >12 mnth, no break, before: red
+anal_brks_wind$plt_pie_event
+
+anal_brks = analyse_brks(brks)
+names(anal_brks)
+
+
+
+# get values in no wind areas
+no_wind = sf::st_difference(x = st_as_sfc(st_bbox(wind)), y = st_union(wind))
+brks_nowind = brks[no_wind]
+anal_brks_nowind = analyse_brks(brks = brks_nowind)
+
+anal_brks_nowind$plt_hist
+
+
+df_brk = data.frame(cls = c("n_brk", "n_no_brk"), 
+           freq = c(anal_brks_nowind$cnt$cnt_brk, 
+                   n_nobrk = anal_brks_nowind$cnt$cnt_no_brk))
+
+# this has to be in percent
+ggplot(df_brk, aes(x="", y=freq, fill=cls)) +
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0)  
+
+
+# to reduce the detections
+# check: 
+#  magnitude
+#  level
+#  influence of more years as history
+
+# repeat this yearly and actually only estimate for a year
+
+
